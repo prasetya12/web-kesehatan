419223ae4f17251e989c38850f9cc11c1100a8ee
prasetya12 <rizalprasetya22@gmail.com>
2022-12-17 10:06:44 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -9,12 +9,9 @@ import {
 } from "react-router-dom";
 import Home from '../src/pages/Home'
 import Artikel from '../src/pages/Artikel'
-import Kategori from './pages/Kategori';
-import DetailKategori from './pages/DetailKategori';
 import Hospital from './pages/Hosptal';
 import DetailHospital from './pages/DetailHospital';
 import TanyaDokter from './pages/TanyaDokter';
-
 
 
 import DetailArtikel from './pages/DetailArtikel';
@@ -29,8 +26,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/artikel" element={<Artikel />} />
-            <Route path="/kategori" element={<Kategori />} />
-            <Route path="/detail" element={<DetailKategori />} />
             <Route path="/hospital" element={<Hospital />} />
             <Route path="/detail-hospital/:id" element={<DetailHospital />} />
             <Route path="/tanya-dokter" element={<TanyaDokter />} />
